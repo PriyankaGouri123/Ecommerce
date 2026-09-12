79b3f90f3dcf46c010b5812ca7eae228a34d266e
Priyanka Gouri <your-email@example.com>
2026-09-12 15:27:30 +0530

    fixed global navbar layout

--- client/src/components/MainLayout.jsx
+++ client/src/components/MainLayout.jsx
@@ -6,6 +6,8 @@ export default function MainLayout() {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
+      {/* Spacer to offset the fixed navbar (announcement bar ~32px + main nav ~60px) */}
+      <div className="pt-[100px]" />
       <div className="flex-grow">
         <Outlet />
       </div>
